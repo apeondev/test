b90613254f4c99253d721347213361fb4f904586
Ritesh Kumar <nyctonio.dev@gmail.com>
2022-10-27 23:37:05 +0530

    monorepo template project

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,4 +2,9 @@ const withTM = require("next-transpile-modules")(["ui"]);
 
 module.exports = withTM({
   reactStrictMode: true,
+  webpack: config => {
+    config.externals = [...(config.externals || []), '@prisma/client'];
+    // Important: return the modified config
+    return config;
+  }
 });
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,54 @@
 import { Button } from "ui";
+import prisma from "database";
 
-export default function Web() {
+export default function Web({ account, webkey }: {
+  account: any,
+  webkey: any
+}) {
   return (
     <div>
-      <h1>Web</h1>
+      {
+        account ? (
+          <div>
+            <h1>Account</h1>
+            {/* account array */}
+            {
+              account.map((item: any) => (
+                <div key={item.id}>
+                  <h2>{item.name}</h2>
+                </div>
+              ))
+            }
+          </div>
+        ) : (
+          <div>
+            <h1>Account</h1>
+            <p>Not found</p>
+          </div>
+        )
+      }
+      <h1>{webkey}</h1>
       <Button />
     </div>
   );
 }
+
+// get server side props
+export async function getServerSideProps() {
+  const webKey = process.env.WEB_KEY;
+  console.log(webKey);
+  await prisma.account.create({
+    data: {
+      name: "hey",
+    },
+  });
+  const account = await prisma.account.findMany();
+  console.log(account);
+  return {
+    props: {
+      account: JSON.parse(JSON.stringify(account)),
+      webkey: "webkey",
+    },
+  };
+}
+
