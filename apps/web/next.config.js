b7fcd5282b39209e1b6ac0e1a9ba20de3001517d
Ritesh Kumar <nyctonio.dev@gmail.com>
2022-10-28 09:00:51 +0530

    monorepo template project

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,18 @@
 const withTM = require("next-transpile-modules")(["ui"]);
+const path = require('path')
 
 module.exports = withTM({
   reactStrictMode: true,
   webpack: config => {
     config.externals = [...(config.externals || []), '@prisma/client'];
+    config.module.rules.push({
+      test: /prisma-client\/index\.js$/,
+      loader: 'string-replace-loader',
+      options: {
+        search: 'config.dirname = dirname',
+        replace: `config.dirname = '${path.dirname(require.resolve('database/generated/prisma-client'))}'`,
+      }
+    })
     // Important: return the modified config
     return config;
   }
